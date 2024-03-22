abde54807a4bce674cd7cee2ecae3fe03d356714
burak balcı <burak.balci@windowslive.com>
2024-03-22 23:01:05 +0300

    Add project files.

---
+++ SessionExtention/SessionExtentionClass.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SessionExtention
+{
+    public static class SessionExtentionClass
+    {
+        public static T GetObject<T>(this ISession session,string key) where T : class
+        {
+            string jsonObject = session.GetString(key);
+            if (jsonObject != null)
+            {
+                T deseObject = JsonConvert.DeserializeObject<T>(jsonObject);
+                return deseObject;
+            }
+            return null;
+        }
+
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            string jsonObject = JsonConvert.SerializeObject(value);
+            session.SetString(key, jsonObject);
+           
+        }
+    }
+}
